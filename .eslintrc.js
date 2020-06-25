0475236c25c3f482c16ea92236f7a0c6c46f01a6
Tom French <tom@tomfren.ch>
2020-06-25 23:49:16 +0100

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: ['airbnb', 'plugin:prettier/recommended', 'prettier/react'],
+    rules: {
+        'import/prefer-default-export': 'off',
+        'no-console': 'off',
+        'prefer-destructuring': 'off',
+        'prefer-template': 'off',
+        'react/destructuring-assignment': 'off'
+    }
+};
