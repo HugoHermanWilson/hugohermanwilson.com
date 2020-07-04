617bbf910ed28baf99f5a5919105f30aaa8366cf
Sarah French <sarah@sarahfrench.co.uk>
2020-07-05 00:59:59 +0100

    Fix styling on biography item lists mob vs desktop

--- src/pages/biography.jsx
+++ src/pages/biography.jsx
@@ -54,16 +54,18 @@ export default function Biography(props) {
                             }}
                         />
                     </div>
-                    <h3 className="mb-10">Recent solo highlights include:</h3>
-                    {/* Components made from /src/pages/biography/solos/operas  */}
-                    <div className="left-text mb-15">
-                        <h3>Operas</h3>
-                        {solosList(operaSolos)}
-                    </div>
-                    {/* Components made from /src/pages/biography/solos/recitals  */}
-                    <div className="left-text mb-15">
-                        <h3>Recitals</h3>
-                        {solosList(recitalSolos)}
+                    <p className="mb-10">Recent solo highlights include:</p>
+                    <div className="responsiveColumns">
+                        {/* Components made from /src/pages/biography/solos/operas  */}
+                        <div className="left-text mb-15 column">
+                            <h3 className="title-font center-text">Operas</h3>
+                            {solosList(operaSolos)}
+                        </div>
+                        {/* Components made from /src/pages/biography/solos/recitals  */}
+                        <div className="left-text mb-15 column">
+                            <h3 className="title-font center-text">Recitals</h3>
+                            {solosList(recitalSolos)}
+                        </div>
                     </div>
                 </Body>
             </Template>
