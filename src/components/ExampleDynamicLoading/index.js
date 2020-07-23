8947bc420aa43cb8269ce5ad036591770b210124
Sarah French <sarah@sarahfrench.co.uk>
2020-07-23 22:40:53 +0100

    Implement example gallery components from react-photo-gallery

---
+++ src/components/ExampleDynamicLoading/index.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import Gallery from 'react-photo-gallery';
+import { debounce } from './utils';
+
+function ExampleDynamicLoading({ photos }) {
+    const [images, setImages] = useState(photos.slice(0, 6));
+    const [pageNum, setPageNum] = useState(1);
+    const [loadedAll, setLoadedAll] = useState(false);
+    const TOTAL_PAGES = 3;
+    const loadMorePhotos = debounce(() => {
+        if (pageNum > TOTAL_PAGES) {
+            setLoadedAll(true);
+            return;
+        }
+
+        setImages(
+            images.concat(photos.slice(images.length, images.length + 6))
+        );
+        setPageNum(pageNum + 1);
+    }, 200);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    });
+
+    const handleScroll = () => {
+        const scrollY =
+            window.scrollY ||
+            window.pageYOffset ||
+            document.documentElement.scrollTop;
+        if (window.innerHeight + scrollY >= document.body.offsetHeight - 50) {
+            loadMorePhotos();
+        }
+    };
+
+    return (
+        <div>
+            <h2>Loading Photos Dynamically in Column Layout</h2>
+            <Gallery photos={images} direction="column" />
+            {!loadedAll && (
+                <div className="loading-msg" id="msg-loading-more">
+                    Loading
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default ExampleDynamicLoading;
---
+++ src/components/HybridGallery/index.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import Gallery from 'react-photo-gallery';
+import Carousel, { Modal, ModalGateway } from 'react-images';
+import { debounce } from './utils';
+
+function ExampleDynamicLoading({ photos }) {
+    const [images, setImages] = useState(photos.slice(0, 6));
+    const [pageNum, setPageNum] = useState(1);
+    const [loadedAll, setLoadedAll] = useState(false);
+    const TOTAL_PAGES = 3;
+    const loadMorePhotos = debounce(() => {
+        if (pageNum > TOTAL_PAGES) {
+            setLoadedAll(true);
+            return;
+        }
+
+        setImages(
+            images.concat(photos.slice(images.length, images.length + 6))
+        );
+        setPageNum(pageNum + 1);
+    }, 200);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    });
+
+    const handleScroll = () => {
+        const scrollY =
+            window.scrollY ||
+            window.pageYOffset ||
+            document.documentElement.scrollTop;
+        if (window.innerHeight + scrollY >= document.body.offsetHeight - 50) {
+            loadMorePhotos();
+        }
+    };
+
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, []);
+
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
+
+    return (
+        <div>
+            <Gallery photos={images} onClick={openLightbox} />
+            {!loadedAll && (
+                <div className="loading-msg" id="msg-loading-more">
+                    Loading
+                </div>
+            )}
+            <ModalGateway>
+                {viewerIsOpen ? (
+                    <Modal onClose={closeLightbox}>
+                        <Carousel
+                            currentIndex={currentImage}
+                            views={photos.map(x => ({
+                                ...x,
+                                srcset: x.srcSet,
+                                caption: x.title
+                            }))}
+                        />
+                    </Modal>
+                ) : null}
+            </ModalGateway>
+        </div>
+    );
+}
+
+export default ExampleDynamicLoading;
