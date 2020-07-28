74f94b1a4515476fed86a69816c9a12985099d39
Sarah French <sarah@sarahfrench.co.uk>
2020-07-29 00:19:31 +0100

    Change file to .jsx extension

--- src/components/HybridGallery/index.jsx
+++ src/components/HybridGallery/index.jsx
@@ -1,9 +1,16 @@
+/* eslint-disable no-undef */
+/*
+ * Adapted from the lightbox and dynamic loading example components in:
+ * https://github.com/neptunian/react-photo-gallery/tree/0bb8e4c4a027c021f8a5a06de71e89026596fd95/examples/src
+ */
+
 import React, { useState, useEffect, useCallback } from 'react';
+import PropTypes from 'prop-types';
 import Gallery from 'react-photo-gallery';
 import Carousel, { Modal, ModalGateway } from 'react-images';
 import { debounce } from './utils';
 
-function ExampleDynamicLoading({ photos }) {
+function HybridGallery({ photos }) {
     const [images, setImages] = useState(photos.slice(0, 6));
     const [pageNum, setPageNum] = useState(1);
     const [loadedAll, setLoadedAll] = useState(false);
@@ -20,25 +27,33 @@ function ExampleDynamicLoading({ photos }) {
         setPageNum(pageNum + 1);
     }, 200);
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    });
-
     const handleScroll = () => {
         const scrollY =
             window.scrollY ||
             window.pageYOffset ||
             document.documentElement.scrollTop;
-        if (window.innerHeight + scrollY >= document.body.offsetHeight - 50) {
+        if (
+            !loadedAll &&
+            window.innerHeight + scrollY >= document.body.offsetHeight - 50
+        ) {
             loadMorePhotos();
         }
     };
 
+    useEffect(() => {
+        if (window) {
+            window.addEventListener('scroll', handleScroll);
+            return () => window.removeEventListener('scroll', handleScroll);
+        }
+        return () => {
+            console.log('Window object not detected');
+        };
+    });
+
     const [currentImage, setCurrentImage] = useState(0);
     const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
-    const openLightbox = useCallback((event, { photo, index }) => {
+    const openLightbox = useCallback((event, { index }) => {
         setCurrentImage(index);
         setViewerIsOpen(true);
     }, []);
@@ -69,4 +84,21 @@ function ExampleDynamicLoading({ photos }) {
     );
 }
 
-export default ExampleDynamicLoading;
+export default HybridGallery;
+
+HybridGallery.propTypes = {
+    photos: PropTypes.arrayOf(
+        PropTypes.shape({
+            key: PropTypes.string,
+            title: PropTypes.string,
+            src: PropTypes.string,
+            srcSet: PropTypes.string,
+            height: PropTypes.number,
+            width: PropTypes.number
+        })
+    )
+};
+
+HybridGallery.defaultProps = {
+    photos: []
+};
