46db07ec2f0311bc9f135fcc2d4982b3d508a4af
Sarah French <sarah@sarahfrench.co.uk>
2020-08-02 12:35:23 +0100

    Swap to using UTC version of moment, remove use of look behinds in regex

--- src/components/Event/utils.js
+++ src/components/Event/utils.js
@@ -1,10 +1,9 @@
 import moment from 'moment';
 
-moment().format();
-
 const dateNumber = date => {
     if (/^\d{4}-\d{2}-\d{2}/.test(date)) {
-        const dateNumberString = /(?<=-)\d{2}(?=T)/.exec(date)[0];
+        const dateNumberString = `${moment.utc(date).date()}`;
+
         if (dateNumberString[0] === '1' || dateNumberString[1] === '1') {
             return dateNumberString[0] + ' ' + dateNumberString[1]; // dealing with non monospaced font
         }
@@ -15,12 +14,12 @@ const dateNumber = date => {
 };
 
 const dateMonth = date => {
-    const month = moment(date).format('MMM');
+    const month = moment.utc(date).format('MMM');
     return month === 'Invalid date' ? 'XXX' : month;
 };
 
 const dateString = date => {
-    const string = moment(date).format('h:mm a, ddd Do MMMM');
+    const string = moment.utc(date).format('h:mm a, ddd Do MMMM');
     return string;
 };
 
